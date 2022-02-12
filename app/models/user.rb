847b31146d89bc9bf45f62bc3b5e0eecb8f463fd
jibiking <daiki.jibiki@gmail.com>
2022-02-12 12:10:22 +0900

    feat: enumで管理者権限の追加 #10

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :name, uniqueness: true
+
+  enum role: { general: 0, admin: 1 }
 end
